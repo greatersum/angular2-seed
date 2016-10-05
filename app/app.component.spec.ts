1fe183f4f2654811eb775c0506bc57075772a0aa
Greater Sum Team <dev01@greatersum.com>
2016-10-05 09:37:07 -0400

    add test for setup

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -1,5 +1,18 @@
+import { AppComponent } from "./app.component";
+import { TestBed, async, ComponentFixture } from "@angular/core/testing";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
+
 describe("test", () => {
+  beforeEach(async(()=> {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent]
+    });
+  }));
+
   it("should test setup", () => {
-    expect(true).toBeTruthy();
+    let fixture: ComponentFixture<AppComponent> = TestBed.createComponent(AppComponent);
+    let element: DebugElement = fixture.debugElement.query(By.css("h1"));
+    expect(element.nativeElement.innerText).toEqual("Greater Sum Angular2 Seed");
   });
 });
--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function (config) {
       "node_modules/zone.js/dist/zone.js",
       "node_modules/zone.js/dist/proxy.js",
       "node_modules/zone.js/dist/sync-test.js",
+      "node_modules/zone.js/dist/async-test.js",
       "node_modules/zone.js/dist/jasmine-patch.js",
       "node_modules/reflect-metadata/Reflect.js",
       {pattern: "node_modules/rxjs/**/*.js", included: false, watched: false},
